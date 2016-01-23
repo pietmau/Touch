fdbbfb7bc6bc75561d0e67a1cf1da49927c40976
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-01-23 20:21:40 +0100

    OK ladscape

--- app/src/main/java/com/pietrantuono/image/MainActivity.java
+++ app/src/main/java/com/pietrantuono/image/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     private int imageHeight;
     private int imageWidth;
-    private Matrix displayMatrix;
     private MultiGestureDetector multiGestureDetector;
     private float previousAngle;
 
@@ -54,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(imageView.getDrawable()!=null)multiGestureDetector.onTouchEvent(event);
+                if (imageView.getDrawable() != null) multiGestureDetector.onTouchEvent(event);
                 return true;
             }
         });
@@ -79,15 +78,14 @@ public class MainActivity extends AppCompatActivity {
                 onEndRotation();
             }
         }, imageView);
-
     }
 
     @DebugLog
     private void translateImage(float distanceX, float distanceY) {
-        Matrix matrix = imageView.getImageMatrix();
-        displayMatrix.set(matrix);
-        displayMatrix.postTranslate(-distanceX, -distanceY);
-        imageView.setImageMatrix(displayMatrix);
+        Matrix matrix = new Matrix();
+        matrix.set(imageView.getImageMatrix());
+        matrix.postTranslate(-distanceX, -distanceY);
+        imageView.setImageMatrix(matrix);
     }
 
     @DebugLog
@@ -121,10 +119,27 @@ public class MainActivity extends AppCompatActivity {
                     imageHeight = imageView.getDrawable().getIntrinsicHeight();
                     RectF drawableRect = new RectF(0, 0, imageWidth, imageHeight);
                     RectF viewRect = new RectF(0, 0, imageView.getWidth(), imageView.getHeight());
-                    Matrix matrix = new Matrix();
-                    matrix.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
-                    displayMatrix = new Matrix();
-                    displayMatrix.set(matrix);
+
+                    //RectF destination = new RectF();
+
+                    if (drawableRect.width() > drawableRect.height())
+                    //Bitmap is landscape
+                    {
+                        float scale=viewRect.height()/drawableRect.height();
+                        Matrix matrix = new Matrix();
+                        //matrix.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
+                        matrix.postScale(scale, scale);
+                        RectF destination =new RectF();
+                        matrix.mapRect(destination,drawableRect);
+                        matrix.postTranslate(-destination.width()/2+viewRect.width()/2,0);
+                        imageView.setImageMatrix(matrix);
+
+                    }
+                    //Bitmap is portrait
+                    else {
+                    }
+
+
                 }
         }
     }
